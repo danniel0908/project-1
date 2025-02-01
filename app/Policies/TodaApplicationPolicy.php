35ac627ed791acbdcf184b76b459a5bf01e4f04a
danniel0908 <159111543+danniel0908@users.noreply.github.com>
2025-02-01 20:07:46 +0800

    Update and rename TODAApplicationPolicy.php to TodaApplicationPolicy.php

--- app/Policies/TodaApplicationPolicy.php
+++ app/Policies/TodaApplicationPolicy.php
@@ -6,7 +6,7 @@ use App\Models\TODAapplication;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class TODAApplicationPolicy
+class TodaApplicationPolicy
 {
     /**
      * Determine whether the user can view any models.
